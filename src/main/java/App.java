7d1fa057ccfa0c2e7f531f7c0c8cdeb0aab5138f
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-24 13:00:06 +0300

    added stage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,6 +13,17 @@ import static spark.Spark.*;
 public class App {
 
     public static void main(String[] args){
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        }else {
+            port = 4567;
+        }
+        port(port);
+
         staticFileLocation("/public");
         Hero.setUpNewHero();
         Hero.setUpNewHero1();
